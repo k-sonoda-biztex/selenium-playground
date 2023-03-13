ff1dd253c2b62a4a898d5198fcac730f17a9e65b
Ken Sonoda <79293352+k-sonoda-biztex@users.noreply.github.com>
2023-03-14 00:06:04 +0900

    feat: sign in to Google

--- src/main.rb
+++ src/main.rb
@@ -1,10 +1,35 @@
 # frozen_string_literal: true
 
+# https://www.selenium.dev/documentation/webdriver/bidirectional/
+# https://www.selenium.dev/selenium/docs/api/rb/Selenium/WebDriver/SearchContext.html
+
+EMAIL = "#{ENV.fetch('GOOG_USER')}@gmail.com"
 require 'selenium-webdriver'
 driver = Selenium::WebDriver.for(
-  :remote, url: 'http://localhost:4444', options: Selenium::WebDriver::Options.chrome
+  :remote, url: 'http://localhost:4444', options: Selenium::WebDriver::Options.chrome(
+    args: [
+      # '--headless=new'
+    ],
+    exclude_switches: [
+      # 'enable-automation'
+    ],
+    detach: false
+  )
 )
 
-driver.get 'https://selenium.dev'
+driver.get 'https://www.google.com/account/about/'
+locator = {
+  relative: {
+    partial_link_text: 'Go to Google Account',
+    above: {
+      xpath: '//*[contains(text(), "Sign in to your Google Account")]'
+    }
+  }
+}
+driver.find_element(locator).click
+
+driver.find_element(:css, '[aria-label="Email or phone"]').send_keys(EMAIL, :return)
+
+# binding.irb
 
 driver.quit
